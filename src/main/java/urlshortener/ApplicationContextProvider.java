267e58a8cdf33c261eab41d746756cdada6e5c89
Alvarogd6 <760704@unizar.es>
2020-12-07 15:00:55 +0100

    import component

--- src/main/java/urlshortener/ApplicationContextProvider.java
+++ src/main/java/urlshortener/ApplicationContextProvider.java
@@ -1,6 +1,6 @@
 package urlshortener;
 
-import org.springframework.stereotype.*;
+import org.springframework.stereotype.Component;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
